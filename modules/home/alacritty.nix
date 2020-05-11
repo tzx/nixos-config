371c9109fcb3d9c809b9392f0c5204fed18592a2
Timmy Xiao <timmyxiao1@gmail.com>
2020-05-11 17:57:33 -0400

    Add tomorrow night to alacritty

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -1,7 +1,7 @@
 {
   programs.alacritty.enable = true;
 
-  # YAML can read JSON
+  # YAML can read JSON, which the attribute set converts to
   programs.alacritty.settings = {
     font = {
       normal = {
@@ -15,6 +15,37 @@
       italic = {
         family = "Input Mono";
 	style = "Italic";
+      };
+    };
+    
+    colors = {
+      primary = {
+         background = "#1d1f21";
+         foreground = "#c5c8c6";
+      };
+      cursor = {
+         text = "#1d1f21";
+         cursor = "#ffffff";
+      };
+      normal = {
+         black = "#1d1f21";
+         red = "#cc6666";
+         green = "#b5bd68";
+         yellow = "#e6c547";
+         blue = "#81a2be";
+         magenta = "#b294bb";
+         cyan = "#70c0ba";
+         white = "#373b41";
+      };
+      bright = {
+         black = "#666666";
+         red = "#ff3334";
+         green = "#9ec400";
+         yellow = "#f0c674";
+         blue = "#81a2be";
+         magenta = "#b77ee0";
+         cyan = "#54ced6";
+         white = "#282a2e";
       };
     };
   };
