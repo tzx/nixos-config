6a6e57f83e9bcc36b2914b93293b09057b6e8540
Timmy Xiao <timmyxiao1@gmail.com>
2020-05-12 14:23:44 -0400

    Change alacritty colors to 256

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -17,36 +17,62 @@
 	style = "Italic";
       };
     };
-    
-    colors = {
-      primary = {
-         background = "#1d1f21";
-         foreground = "#c5c8c6";
+
+    colors= {
+      primary= {
+         background = "0x1d1f21";
+         foreground = "0xc5c8c6";
       };
-      cursor = {
-         text = "#1d1f21";
-         cursor = "#ffffff";
+      cursor= {
+         text = "0x1d1f21";
+         cursor = "0xc5c8c6";
       };
-      normal = {
-         black = "#1d1f21";
-         red = "#cc6666";
-         green = "#b5bd68";
-         yellow = "#e6c547";
-         blue = "#81a2be";
-         magenta = "#b294bb";
-         cyan = "#70c0ba";
-         white = "#373b41";
+      normal= {
+         black = "0x1d1f21";
+         red = "0xcc6666";
+         green = "0xb5bd68";
+         yellow = "0xf0c674";
+         blue = "0x81a2be";
+         magenta = "0xb294bb";
+         cyan = "0x8abeb7";
+         white = "0xc5c8c6";
       };
-      bright = {
-         black = "#666666";
-         red = "#ff3334";
-         green = "#9ec400";
-         yellow = "#f0c674";
-         blue = "#81a2be";
-         magenta = "#b77ee0";
-         cyan = "#54ced6";
-         white = "#282a2e";
+      bright= {
+         black = "0x969896";
+         red = "0xcc6666";
+         green = "0xb5bd68";
+         yellow = "0xf0c674";
+         blue = "0x81a2be";
+         magenta = "0xb294bb";
+         cyan = "0x8abeb7";
+         white = "0xffffff";
       };
+      indexed_colors= [
+         {
+            index = 16;
+            color = "0xde935f";
+         }
+         {
+            index = 17;
+            color = "0xa3685a";
+         }
+         {
+            index = 18;
+            color = "0x282a2e";
+         }
+         {
+            index = 19;
+            color = "0x373b41";
+         }
+         {
+            index = 20;
+            color = "0xb4b7b4";
+         }
+         {
+            index = 21;
+            color = "0xe0e0e0";
+         }
+      ];
     };
   };
 }
